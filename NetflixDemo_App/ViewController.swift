e0c0972571e574bd1e15356abbbc348cccf18d71
Николай <imkoddi@mail.ru>
2022-10-07 13:16:18 +0300

    Initial Commit

---
+++ NetflixDemo_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixDemo_App
+//
+//  Created by KODDER on 07.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
