a85037d6497e91b69622b8c0fab4a0d32392c752
Николай <imkoddi@mail.ru>
2022-10-10 12:33:29 +0300

    Sending URL Requests and Parsing JSON response

---
+++ NetflixDemo_App/Managers/APICaller.swift
@@ -0,0 +1,40 @@
+//
+//  APICaller.swift
+//  NetflixDemo_App
+//
+//  Created by KODDER on 10.10.2022.
+//
+
+import UIKit
+
+
+struct Constants {
+    static let API_KEY = "75707bcf3f52a789b5dc3dee9cf12032"
+    static let baseURL = "https://api.themoviedb.org"
+}
+
+enum APIError: Error {
+    case failedTogetData
+}
+
+class APICaller {
+    static let shared = APICaller()
+    
+    
+    func getTrendingMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
+        guard let url = URL(string: "\(Constants.baseURL)/3/trending/all/day?api_key=\(Constants.API_KEY)") else {return}
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            do {
+                let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                completion(.success(results.results))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
+}
