9817dbf7dbce8dcfefdf891b7a581d7ed0466f4a
Nikolay Dobychin <koddek@bk.ru>
2022-10-18 00:21:39 +0300

    Created TitleViewModel Creating custom TableViewCell

---
+++ NetflixDemo_App/ViewModels/TitleViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TitleViewModel.swift
+//  NetflixDemo_App
+//
+//  Created by KODDER on 17.10.2022.
+//
+
+import Foundation
+
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
